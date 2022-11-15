b9c5730b6bcf129dfe7f0f1f82a22e6f0c5b6d19
Rajesh Kumar Thankaraj <rajeshkumarthankaraj@Rajeshs-MacBook-Pro.local>
2022-11-15 16:31:29 +0530

    First Upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserhandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={addUserhandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Users/AddUser.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useRef } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
+import styles from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const nameInputRef = useRef();
+  const ageInputref = useRef();
+
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputref.current.value;
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty value).",
+      });
+      return;
+    }
+    if (+enteredUserAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0)",
+      });
+      return;
+    }
+    props.onAddUser(enteredName, enteredUserAge);
+    nameInputRef.current.value = "";
+    ageInputref.current.value = "";
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" ref={nameInputRef} />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" ref={ageInputref} />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </>
+  );
+};
+
+export default AddUser;
